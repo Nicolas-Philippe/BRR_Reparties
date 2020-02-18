440624f0099ba28b9f19bab365b59f8ad2fd1704
Ilias H <haouchilias@hotmail.com>
2020-02-18 09:08:09 -0400

    Merge branch 'master' of C:\Users\haouc\Desktop\UQAC\base-donnees-repartis\Devoir1\BRR_Reparties with conflicts.

--- build.sbt
+++ build.sbt
@@ -2,14 +2,13 @@ name := "ProjectBDD"
 
 version := "0.1"
 
-scalaVersion := "2.12.0"
+scalaVersion := "2.12.10"
 
 updateOptions := updateOptions.value.withCachedResolution(true)
 
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.4"
 libraryDependencies += "org.apache.spark" %% "spark-graphx" % "2.4.4"
 
-useCoursier := false
 //mainClass in assembly := Some("ca.lif.sparklauncher.app.Application")
 /*mainClass in assembly := Some("cmdline.MainConsole")
 
--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -9,6 +9,7 @@
 
   object freestyle extends App
   {
+
     implicit val codec = Codec("UTF-8")
     codec.onMalformedInput(CodingErrorAction.REPLACE)
     codec.onUnmappableCharacter(CodingErrorAction.REPLACE)
@@ -77,67 +78,66 @@
 
 
 
-/*
+  /*
 
 
-    val conf = new SparkConf()
-      .setAppName("Petit exemple")
-      .setMaster("local[*]")
-    val sc = new SparkContext(conf)
-    sc.setLogLevel("ERROR")
+      val conf = new SparkConf()
+        .setAppName("Petit exemple")
+        .setMaster("local[*]")
+      val sc = new SparkContext(conf)
+      sc.setLogLevel("ERROR")
 
 
-    val tableau: Array[Int] = Array(1,2,3,4,5)
+      val tableau: Array[Int] = Array(1,2,3,4,5)
 
-    val resultatRDD = sc.makeRDD(tableau).map( element => {
-      element * 3
-    }).cache()
+      val resultatRDD = sc.makeRDD(tableau).map( element => {
+        element * 3
+      }).cache()
 
-    resultatRDD.collect().foreach(  e => print(e))*/
-
-/*
-    val tableau: Array[Int] = Array(1,2,3,4,5)
-    println("*****")
-    //On veut garder le plus petit de toute la collection
-    val a =  resultatRDD.reduce(   (a, b) => {
-      if (a < b) a else b
-    })
-
-    println(a)
-
-    val c = resultatRDD.aggregate("")(
-      (acc, nouveau) => acc + nouveau.toString,
-      (a,b) => a.toString + b.toString
-    )
-
-    //Tous les éléments impairs vont avoir la catégorie 2
-    //et les pairs vont avoir la catégorie (clé) 1
-    //On va créer un PairRDD
-    val r1: RDD[(Int, Int)] = resultatRDD.map(elem => {
-      var categorie = 1
-      if (elem % 2 == 1) categorie = 2
-      (categorie, elem)
-    })
-
-    r1.reduceByKey(  (a , b ) =>  a+b).collect()
-      .foreach( e => println(e))
+      resultatRDD.collect().foreach(  e => print(e))
 
 
-    println("Resultats du flatmap")
+      println("*****")
+      //On veut garder le plus petit de toute la collection
+      val a =  resultatRDD.reduce(   (a, b) => {
+        if (a < b) a else b
+      })
 
-    //3 6 9 12 15
-    resultatRDD.flatMap(  elem => {
-      var resultats = new ArrayBuffer[(Int,Int)]()
-      for (i <- 1 to elem) {
+      println(a)
+
+      val c = resultatRDD.aggregate("")(
+        (acc, nouveau) => acc + nouveau.toString,
+        (a,b) => a.toString + b.toString
+      )
+
+      //Tous les éléments impairs vont avoir la catégorie 2
+      //et les pairs vont avoir la catégorie (clé) 1
+      //On va créer un PairRDD
+      val r1: RDD[(Int, Int)] = resultatRDD.map(elem => {
         var categorie = 1
-        if (i % 2 == 1) categorie = 2
-        resultats += Tuple2(elem, i)
-      }
-      resultats
-    }) .collect(). foreach(  e => println(e))
-*/
+        if (elem % 2 == 1) categorie = 2
+        (categorie, elem)
+      })
 
-    //
-    //  println("******")
-    //  print(c)
-  }
+      r1.reduceByKey(  (a , b ) =>  a+b).collect()
+        .foreach( e => println(e))
+
+
+      println("Resultats du flatmap")
+
+      //3 6 9 12 15
+      resultatRDD.flatMap(  elem => {
+        var resultats = new ArrayBuffer[(Int,Int)]()
+        for (i <- 1 to elem) {
+          var categorie = 1
+          if (i % 2 == 1) categorie = 2
+          resultats += Tuple2(elem, i)
+        }
+        resultats
+      }) .collect(). foreach(  e => println(e))
+
+  */
+      //
+      //  println("******")
+      //  print(c)
+    }
